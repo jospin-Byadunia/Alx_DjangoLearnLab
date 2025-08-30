6a73a216867e2a3388f5c22091e3afc68ed1f339
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-08-30 21:44:50 +0200

    Fix list_filter in BookAdmin to use correct field name 'publication_year'

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_filter = ('title', 'author', 'published_date')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
