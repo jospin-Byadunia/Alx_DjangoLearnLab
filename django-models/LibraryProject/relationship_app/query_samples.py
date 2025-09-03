3f1680f5d3305732cbb6362127266c05c6dffdbe
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-03 19:30:55 +0200

    Added some sample queries

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,7 @@
+from models import Author, Book, Library, Librarian
+
+# Sample Queries
+authors = Author.objects.all()
+books = Book.objects.all()
+libraries = Library.objects.all()
+librarians = Librarian.objects.all()
