a878d62bbbbc467eb596a0edefc4b1cecb6ade2e
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-03 19:40:27 +0200

    Refactored the queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,18 @@
 from models import Author, Book, Library, Librarian
 
 # Sample Queries
-authors = Author.objects.all()
-books = Book.objects.all()
-libraries = Library.objects.all()
-librarians = Librarian.objects.all()
+# authors = Author.objects.all()
+# books = Book.objects.all()
+# library = Library.objects.get(name='library_name')
+# books = library.books.all()
+# librarians = Librarian.objects.all()
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+def get_books_by_author(author_name):
+    return Book.objects.filter(author=author_name)
+
+def get_librarian_by_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
