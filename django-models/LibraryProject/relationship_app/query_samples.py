2213c0a362f2c08f28f91a6e3c7939b96d56629d
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-03 19:44:16 +0200

    Corrected the get_book_by_author function

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -11,7 +11,8 @@ def get_books_in_library(library_name):
     return library.books.all()
 
 def get_books_by_author(author_name):
-    return Book.objects.filter(author=author_name)
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 def get_librarian_by_library(library_name):
     library = Library.objects.get(name=library_name)
