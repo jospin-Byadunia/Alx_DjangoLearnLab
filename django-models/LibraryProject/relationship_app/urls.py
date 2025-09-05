4e0c518284e991e55dca4b0c1addd5116722615e
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-05 21:14:06 +0200

    correction on list_books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
-from .views import book_list, LibraryDetailView
+from .views import LibraryDetailView
 from django.urls import path, include
 from django.contrib import admin
+from .views import list_books
 
 
 urlpatterns = [
-    path('book-list/', book_list, name='book_list'),
+    path('book-list/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 # Create your views here.
 
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()  # Optimized query
     return render(request, "relationship_app/list_books.html", {"books": books})
 
