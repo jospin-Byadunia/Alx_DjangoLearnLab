a49301becdc49ffa95094c2ab5a1217625b91746
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-07 10:49:40 +0200

    put Admin

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -27,6 +27,6 @@ class Librarian(models.Model):
     
 class UserProfile(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    role = models.CharField(choices=[('admin', 'admin'),('member', 'Member'), ('librarian', 'Librarian')], max_length=20)
+    role = models.CharField(choices=[('admin', 'Admin'),('member', 'Member'), ('librarian', 'Librarian')], max_length=20)
     def __str__(self):
         return f"{self.user.username}'s profile"
