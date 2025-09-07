a8a4b56d8ecbce0a9dba9b9851cf5089d719c685
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-07 10:09:07 +0200

    refactored views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,13 +2,13 @@ from .views import LibraryDetailView
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
-from .views import SignUpView
+import views
 
 
 urlpatterns = [
     path('book-list/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('signup/', SignUpView.as_view(), name='register'),
+    path("register/", views.register, name="register"),   # <-- match the view name
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
