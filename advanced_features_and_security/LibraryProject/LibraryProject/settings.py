89d6d6ffef62b9c30cd0dd3b7e4efd596bc30815
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-12 20:50:24 +0200

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom user model
-AUTH_USER_MODEL = 'relationship_app.User'
+AUTH_USER_MODEL = "relationship_app.CustomUser"
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -43,6 +43,8 @@ class UserProfile(models.Model):
 class User(AbstractBaseUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    def __str__(self):
+        return self.username
 
 
 ##Create User Manager for Custom User Model
