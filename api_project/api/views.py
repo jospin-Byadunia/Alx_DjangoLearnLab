a2cc201a68bb7409349edb58e111299a8ea41095
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-18 19:34:48 +0200

    Added listAPIview

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,6 @@ from rest_framework import generics
 # Create your views here.
 
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
