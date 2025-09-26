4daaa84403d52040d2a682acd172a705afe48623
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-26 21:32:32 +0200

    Add API URL routing and implement CRUD views for Book model with permissions

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,14 @@
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import path
+
+
+urlpatterns = [
+    path('books/', ListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),
+]
+
+
+
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, Author    
+from .serializers import BookSerializer, AuthorSerializer, serializers
+from rest_framework.response import Response
+from rest_framework import permissions
 
-# Create your views here.
+# list all books
+
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes =[permissions.IsAuthenticatedOrReadOnly]
+
+# detail view of a book
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes =[permissions.IsAuthenticatedOrReadOnly]
+    
+# Adding a new book
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Allow any user (authenticated or not) to access this view
+    
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        if Book.objects.filter(title=title).exists():
+            raise serializers.ValidationError({"title": "A book with this title already exists."})
+        
+        # Save with additional field (if your Book model has a 'created_by' field for example)
+        serializer.save(created_by=self.request.user)
+
+# Updating a book
+class UpdateView (generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        # Example: Only allow the original creator to edit the book
+        book = self.get_object()
+        if book.created_by != self.request.user:
+            raise permissions.PermissionDenied("You do not have permission to edit this book.")
+
+        serializer.save()
+    
+# Deleting a book
+class DeleteView (generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # ✅ Only admins can delete
