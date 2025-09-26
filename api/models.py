ee231931250c73cf7296f7f1bed38fb256463ccd
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-26 19:07:34 +0200

    Add initial Django project files including settings, URLs, ASGI, WSGI, and remove unused migrations and serializers

--- api/models.py
+++ api/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
-# Model for Author
-
-
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-# Model for Book with ForeignKey to Author
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(
-        Author, related_name='books', on_delete=models.CASCADE)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return self.title
+# Create your models here.
