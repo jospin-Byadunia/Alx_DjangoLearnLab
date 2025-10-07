15be5927c834443dfe7bbf69bd9330e61e1948a2
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-10-07 20:14:45 +0200

    replaced to create_user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -33,14 +33,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
         )
-        user.set_password(validated_data['password'])
-        user.save()
 
         # Create DRF token
         Token.objects.create(user=user)
